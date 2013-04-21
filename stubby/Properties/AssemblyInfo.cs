90f34387c33a70794af97956ed02318320774442
Eric Mrak <enmrak@gmail.com>
2013-04-21 05:39:09 -0700

    package management for nuget and chocolatey updates

--- stubby/Properties/AssemblyInfo.cs
+++ stubby/Properties/AssemblyInfo.cs
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
 
 
 // Make other asseblies "friendly" (able to access internal classes of this assembly)
