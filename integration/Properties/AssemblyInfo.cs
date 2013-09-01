3745d3e640bfbce0589cd534cb725fa2687e2b6f
Eric Mrak <enmrak@gmail.com>
2013-09-01 11:20:31 -0700

    monodevelop formatter messed up AssemblyInfo.cs files

--- integration/Properties/AssemblyInfo.cs
+++ integration/Properties/AssemblyInfo.cs
@@ -5,20 +5,23 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("integration")
-[assembly: AssemblyDescription("")
-[assembly: AssemblyConfiguration("")
-[assembly: AssemblyCompany("")
-[assembly: AssemblyProduct("integration")
-[assembly: AssemblyCopyright("Copyright ©  2013")
-[assembly: AssemblyTrademark("")
-[assembly: AssemblyCulture("")
+[assembly: AssemblyTitle("integration")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("integration")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)
+[assembly: ComVisible(false)]
+
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("026e8174-87bb-459d-b0e6-9c6c00b20ea0")
+[assembly: Guid("026e8174-87bb-459d-b0e6-9c6c00b20ea0")]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -29,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- unit/Properties/AssemblyInfo.cs
+++ unit/Properties/AssemblyInfo.cs
@@ -4,20 +4,26 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("unit")
-[assembly: AssemblyDescription("")
-[assembly: AssemblyConfiguration("")
-[assembly: AssemblyCompany("")
-[assembly: AssemblyProduct("unit")
-[assembly: AssemblyCopyright("Copyright ©  2012")
-[assembly: AssemblyTrademark("")
-[assembly: AssemblyCulture("")
+
+[assembly: AssemblyTitle("unit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("unit")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)
+
+[assembly: ComVisible(false)]
+
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9240eb10-a2cb-422d-8955-fa23df54822c")
+
+[assembly: Guid("9240eb10-a2cb-422d-8955-fa23df54822c")]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -28,5 +34,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")
+
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
